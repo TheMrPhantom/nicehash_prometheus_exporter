ad8afb6f699d423c543e6a801f7f66cb34e7b4d1
schieljn <diemilbe@gmx.net>
2021-06-12 16:33:50 +0200

    fix

--- example.config.py
+++ example.config.py
@@ -1,6 +1,6 @@
 import os
 
-port= os.environ.get("PORT") if os.environ.get("PORT") else 8080
+port= int(os.environ.get("PORT")) if os.environ.get("PORT") else 8080
 api_url= os.environ.get("API_URL") if os.environ.get("API_URL") else "https://api2.nicehash.com"
 api_url_prefix= os.environ.get("API_URL_PREFIX") if os.environ.get("API_URL_PREFIX") else "/main/api/v2"
 organization_id= os.environ.get("ORGANIZATION_ID") if os.environ.get("ORGANIZATION_ID") else ""
