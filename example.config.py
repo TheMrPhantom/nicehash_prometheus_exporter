12e5b81cf5d2c4daf71ae8ef678ccddd47d33fbc
schieljn <diemilbe@gmx.net>
2021-06-12 16:03:11 +0200

    Added docker

--- example.config.py
+++ example.config.py
@@ -1,6 +1,8 @@
-port=8001
-api_url="https://api2.nicehash.com"
-api_url_prefix="/main/api/v2"
-organization_id=""
-key=""
-key_secret=""
+import os
+
+port= os.environ.get("PORT") if os.environ.get("PORT") else 8080
+api_url= os.environ.get("API_URL") if os.environ.get("API_URL") else "https://api2.nicehash.com"
+api_url_prefix= os.environ.get("API_URL_PREFIX") if os.environ.get("API_URL_PREFIX") else "/main/api/v2"
+organization_id= os.environ.get("ORGANIZATION_ID") if os.environ.get("ORGANIZATION_ID") else ""
+key= os.environ.get("KEY") if os.environ.get("KEY") else ""
+key_secret= os.environ.get("KEY_SECRET") if os.environ.get("KEY_SECRET") else ""
